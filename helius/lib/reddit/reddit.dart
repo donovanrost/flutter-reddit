02c151e767192a0e1e25bc7b5b4b5f10ab2d2e38
Donovan Rost <drost@ucsc.edu>
2019-05-21 11:15:30 -0700

    rm-ing deleted files

--- helius/lib/reddit/reddit.dart
+++
@@ -1,3 +0,0 @@
-export 'reddit_bloc.dart';
-export 'reddit_event.dart';
-export 'reddit_state.dart';
--- helius/lib/settings/settings_event.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:equatable/equatable.dart';
-
-abstract class SettingsEvent extends Equatable {}
-
-class DummySettingEvent extends SettingsEvent{
-  
-}
